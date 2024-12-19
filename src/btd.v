b1aea060ade97a9ee9e05e6d142cb14855de1a09
jige <211163032@qq.com>
2024-12-20 00:06:09 +0800

    btd解码器切片修改

--- src/btd.v
+++ src/btd.v
@@ -13,10 +13,10 @@ integer j;
         
         for (i = 19;i >= 0; i = i-1) begin
             for (j = 0; j <= 5; j = j+1) begin
-                if (register[(4*j)+3:4]>= 4'b0101) register[(4*j)+3:4] = register[(4*j)+3:4] + 4'b0011;
+                if (register[(4*j)+3-:4]>= 4'b0101) register[(4*j)+3-:4] = register[(4*j)+3-:4] + 4'b0011;
             end
             for (j = 1; j <= 5; j = j+1) begin
-                register[(4*j)+3:4] = {register[4j+:2], register[4j-1]};
+                register[(4*j)+3-:4] =  {register[(4*j-1)+3-:2], register[(4*j-1)+1 -:1]};
             end
             register[3:0] <= {register[2:0], binary[i]};
 
