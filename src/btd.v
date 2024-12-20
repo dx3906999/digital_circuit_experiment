8e1fa20f1ecf1aca758fc098c533c057cbfed4e8
jige <211163032@qq.com>
2024-12-20 23:31:10 +0800

    debug

--- src/btd.v
+++ src/btd.v
@@ -15,10 +15,7 @@ integer j;
             for (j = 0; j <= 5; j = j+1) begin
                 if (register[(4*j)+3-:4]>= 4'b0101) register[(4*j)+3-:4] = register[(4*j)+3-:4] + 4'b0011;
             end
-            for (j = 1; j <= 5; j = j+1) begin
-                register[(4*j)+3-:4] = {register[(4*j-1)+3-:2], register[(4*j-1)+1 -:1]};
-            end
-            register[3:0] = {register[2:0], binary[i]};//使用阻塞赋值
+            register = {register[22:0], binary[i]};//使用阻塞赋值
 
         end
     end
